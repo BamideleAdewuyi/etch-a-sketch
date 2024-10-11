996a7b4987f2dbf23302e54da3dbbb430e4cbaad
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-11 01:39:05 +0100

    main.js: Wrote createGrid function. Also changed container const so it references id of container (#container)

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 // Container
-const container = document.querySelector("container");
+const container = document.querySelector("#container");
 
 // Function for creating grid
 function createGrid(squares) {
+    const toAdd = document.createDocumentFragment();
+    for (i = 0; i != squares; i++) {
+        const newSquare = document.createElement("div");
+        newSquare.id = "square" + i;
+        newSquare.className = "square";
+        toAdd.appendChild(newSquare);
+    }
+    container.appendChild(toAdd);
     
 }
+
+createGrid(16);
