3ee9e7c2b1eb26e6c6db902cca8b4d3a236b334b
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-13 01:19:11 +0100

    main.js: Edited createGrid to define height and width of squares

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ function createGrid(squares) {
         const newSquare = document.createElement("div");
         newSquare.id = "square" + i;
         newSquare.className = "square";
+        newSquare.style.height = window.innerHeight/squares +"px";
+        newSquare.style.width = window.innerWidth/squares + "px";
         toAdd.appendChild(newSquare);
     }
     container.appendChild(toAdd);
@@ -15,3 +17,6 @@ function createGrid(squares) {
 }
 
 createGrid(16);
+console.log("Height, width: " + window.innerHeight/16, window.innerWidth/16)
+console.log(document.getElementById("square4").style.height)
+// Height: 59.875 Width: 17.5625
