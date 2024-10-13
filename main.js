8225a301bb91d3d39ba96e32a29b42ee7ee8b135
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-13 20:06:48 +0100

    main.js: Edited createGrid so size of squares is calculated by dividing 500 (width and height of container) by the argument entered when calling function

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ const container = document.querySelector("#container");
 // Function for creating grid
 function createGrid(squares) {
     const toAdd = document.createDocumentFragment();
-    for (i = 0; i != squares; i++) {
+    for (i = 0; i != squares * squares; i++) {
         const newSquare = document.createElement("div");
         newSquare.id = "square" + i;
         newSquare.className = "square";
-        newSquare.style.height = window.innerHeight/squares +"px";
-        newSquare.style.width = window.innerWidth/squares + "px";
+        newSquare.style.height = 500/squares + "px";
+        newSquare.style.width = 500/squares +"px";
         toAdd.appendChild(newSquare);
     }
     container.appendChild(toAdd);
-    
 }
 
 createGrid(16);
 console.log("Height, width: " + window.innerHeight/16, window.innerWidth/16)
-console.log(document.getElementById("square4").style.height)
+// console.log(document.getElementById("square4").style.height);
+console.log(document.getElementById("container").style.width)
 // Height: 59.875 Width: 17.5625
