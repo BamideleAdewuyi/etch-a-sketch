b2995ddbe594730c429e24ba234800e19ca37f9d
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-13 20:56:48 +0100

    main.js: Added event handler to reset button so user can enter new number of squares to grid. Grid then updates with new number

--- main.js
+++ main.js
@@ -14,6 +14,31 @@ function createGrid(squares) {
         toAdd.appendChild(newSquare);
     }
     container.appendChild(toAdd);
-}
+};
+
+// Button listener
+reset.addEventListener("click", () => {
+    let num = prompt("Enter number of squares for new grid between 1 and 100");
+    while (document.getElementsByClassName('square')[0]) {
+        document.getElementsByClassName('square')[0].remove();
+    }
+    createGrid(num);
+});
+
+
+// restart.addEventListener("click", () => {
+//     humanScore = 0;
+//     computerScore = 0;
+//     scores.textContent = "Player Score: 0\nComputer Score: 0";
+//     // Make buttons reappear
+//     buttons.forEach((button) => {
+//         document.getElementById(button.id).style.visibility = "visible";
+//     })
+//     results.textContent = "Results of each round will appear here";
+//     restart.remove();
+// });
+
+
+
 
 createGrid(16);
